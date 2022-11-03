0af7003846d22b996cfb678fac65734ef68ef0f0
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-11-02 22:38:06 -0700

    WIP implementation of polygon triangulation

--- lib/util.ml
+++ lib/util.ml
@@ -20,7 +20,7 @@ let rev_array arr =
 
 let array_of_list_rev l =
   match l with
-  | []       -> [||]
+  | [] -> [||]
   | hd :: tl ->
     let len = 1 + List.length tl in
     let a = Array.make len hd
@@ -28,7 +28,7 @@ let array_of_list_rev l =
     (* Start at [len - 2] as [make] has placed [hd] at [t.(len - 1)]. *)
     for i = len - 2 downto 0 do
       match !r with
-      | []       -> assert false
+      | [] -> assert false
       | hd :: tl ->
         a.(i) <- hd;
         r := tl
@@ -37,14 +37,14 @@ let array_of_list_rev l =
 
 let array_of_list_map f l =
   match l with
-  | []       -> [||]
+  | [] -> [||]
   | hd :: tl ->
     let len = 1 + List.length tl in
     let a = Array.make len (f hd)
     and r = ref tl in
     for i = 1 to len - 1 do
       match !r with
-      | []       -> assert false
+      | [] -> assert false
       | hd :: tl ->
         a.(i) <- f hd;
         r := tl
@@ -53,14 +53,14 @@ let array_of_list_map f l =
 
 let array_of_list_rev_map f l =
   match l with
-  | []       -> [||]
+  | [] -> [||]
   | hd :: tl ->
     let len = 1 + List.length tl in
     let a = Array.make len (f hd)
     and r = ref tl in
     for i = len - 2 downto 0 do
       match !r with
-      | []       -> assert false
+      | [] -> assert false
       | hd :: tl ->
         a.(i) <- f hd;
         r := tl
@@ -69,14 +69,14 @@ let array_of_list_rev_map f l =
 
 let array_of_list_mapi f l =
   match l with
-  | []       -> [||]
+  | [] -> [||]
   | hd :: tl ->
     let len = 1 + List.length tl in
     let a = Array.make len (f 0 hd)
     and r = ref tl in
     for i = 1 to len - 1 do
       match !r with
-      | []       -> assert false
+      | [] -> assert false
       | hd :: tl ->
         a.(i) <- f i hd;
         r := tl
@@ -85,14 +85,14 @@ let array_of_list_mapi f l =
 
 let array_of_list_rev_mapi f l =
   match l with
-  | []       -> [||]
+  | [] -> [||]
   | hd :: tl ->
     let len = 1 + List.length tl in
     let a = Array.make len (f 0 hd)
     and r = ref tl in
     for i = len - 2 downto 0 do
       match !r with
-      | []       -> assert false
+      | [] -> assert false
       | hd :: tl ->
         a.(i) <- f i hd;
         r := tl
@@ -136,11 +136,11 @@ let array_all_equal f a =
 
 let value_map_opt ~default f = function
   | Some a -> f a
-  | None   -> default
+  | None -> default
 
 let unzip l =
   let rec loop l1 l2 = function
-    | []             -> l1, l2
+    | [] -> l1, l2
     | (h1, h2) :: tl -> loop (h1 :: l1) (h2 :: l2) tl
   in
   loop [] [] (List.rev l)
@@ -169,20 +169,20 @@ let prepend_init n f init = fold_init n (fun i acc -> f i :: acc) init
 let fold3 f init l1 l2 l3 =
   let rec loop acc l1 l2 l3 =
     match l1, l2, l3 with
-    | [], [], []                   -> acc
+    | [], [], [] -> acc
     | h1 :: t1, h2 :: t2, h3 :: t3 -> loop (f acc h1 h2 h3) t1 t2 t3
-    | _                            -> invalid_arg "Util.fold3: Unequal lengths."
+    | _ -> invalid_arg "Util.fold3: Unequal lengths."
   in
   loop init l1 l2 l3
 
 let last_element = function
-  | []       -> invalid_arg "No last element in empty list."
+  | [] -> invalid_arg "No last element in empty list."
   | hd :: tl -> List.fold_left (fun _ e -> e) hd tl
 
 let prepend_opt opt l =
   match opt with
   | Some a -> a :: l
-  | None   -> l
+  | None -> l
 
 let helix_arc_length ~height ~r twist =
   twist *. Float.(sqrt ((r *. r) +. pow (height /. twist) 2.))
@@ -194,18 +194,18 @@ let helical_slices ?fn ?(fa = fa) twist =
   let min_slices = Int.max 1 Float.(twist /. (pi /. 3.) |> ceil |> to_int) in
   ( match fn with
   | Some n -> Float.(Float.of_int n *. twist /. (2. *. pi) |> ceil |> to_int)
-  | None   -> Float.(to_int @@ ceil (twist /. fa)) )
+  | None -> Float.(to_int @@ ceil (twist /. fa)) )
   |> Int.max min_slices
 
 let helical_fragments ?fn ?(fa = fa) ?(fs = fs) radius =
   match fn with
   | Some n -> Int.max 3 n
-  | None   ->
+  | None ->
     Float.(to_int @@ max (ceil @@ min (2. *. pi /. fa) (radius *. pi *. 2. /. fs)) 5.)
 
 let getter ~len ~name = function
   | `Flat n -> Fun.const n
-  | `Mix l  ->
+  | `Mix l ->
     let a = Array.of_list l in
     if Array.length a <> len
     then
